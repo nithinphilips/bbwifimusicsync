ec101dd2546f99cab63315b3c5e2d6355f325619
Nithin Philips <nithin@nithinphilips.com>
2011-01-12 22:02:20 +0000

    * Moving shared code to a library.

---
+++ libMusicSync/iTunesExport/Parser/IPlaylist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace iTunesExport.Parser
+{
+    public interface IPlaylist
+    {
+        bool Folder { get; }
+        int Id { get; }
+        string Name { get; }
+        IEnumerable<ITrack> Tracks { get; }
+    }
+}

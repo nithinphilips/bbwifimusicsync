165b95a44eebddd462e13583e2df545e835ef8f7
Nithin Philips <nithin@nithinphilips.com>
2011-01-13 03:22:41 +0000

    * Moving shared code to a library & Cleanup.

---
+++ MusicSyncDesktop/Model/CopyProgressModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using WifiSyncDesktop.Helpers;
+
+namespace WifiSyncDesktop.Model
+{
+    [NotifyPropertyChanged]
+    public class CopyProgressModel
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Size { get; set; }
+        public int Percentage { get; set; }
+    }
+}
---
+++ MusicSyncDesktop/Threading/FileCopier.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LibQdownloader.Threading;
+using System.Threading;
+using System.IO;
+
+namespace WifiSyncDesktopClient.Threading
+{
+    class FileCopier : IDisposable, IPauseable
+    {
+
+        public bool Paused
+        {
+            get { return false; }
+        }
+
+
+        #region IPauseable Members
+
+        public void Pause()
+        {
+            
+        }
+
+        public void Resume()
+        {
+            
+        }
+
+        #endregion
+
+        public void Work(FileCopyJob job)
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(job.Destination));
+            File.Copy(job.Source, job.Destination, true);
+        }
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            
+        }
+
+        #endregion
+    }
+}

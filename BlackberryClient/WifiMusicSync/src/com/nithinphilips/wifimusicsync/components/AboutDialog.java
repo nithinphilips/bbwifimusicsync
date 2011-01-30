17946aef0f233617002469f6074932ac65942795
Nithin Philips <nithin@nithinphilips.com>
2011-01-30 06:21:14 +0000

    * Updating about dialog.

--- BlackberryClient/WifiMusicSync/src/com/nithinphilips/wifimusicsync/components/AboutDialog.java
+++ BlackberryClient/WifiMusicSync/src/com/nithinphilips/wifimusicsync/components/AboutDialog.java
@@ -11,15 +11,16 @@ import net.rim.device.api.ui.component.LabelField;
 import net.rim.device.api.ui.component.RichTextField;
 import net.rim.device.api.ui.component.SeparatorField;
 import net.rim.device.api.ui.container.HorizontalFieldManager;
+import net.rim.device.api.ui.container.MainScreen;
 import net.rim.device.api.ui.container.PopupScreen;
 import net.rim.device.api.ui.container.VerticalFieldManager;
 
 import com.nithinphilips.wifimusicsync.model.PlaylistInfo;
 
-public class AboutDialog extends PopupScreen {
+public class AboutDialog extends MainScreen {
 
     public AboutDialog() {
-        super(new VerticalFieldManager(VERTICAL_SCROLL | VERTICAL_SCROLLBAR));
+        //super(new VerticalFieldManager(VERTICAL_SCROLL | VERTICAL_SCROLLBAR));
     
         RichTextField popupTitleField = new RichTextField("About Music Sync", RichTextField.NON_FOCUSABLE);
 
@@ -38,18 +39,10 @@ public class AboutDialog extends PopupScreen {
 
         add(popupTitleField);
         add(new SeparatorField());
-        add(new LabelField("Revision $Rev$"));
+        add(new LabelField("$Date$ $Rev$"));
         
         buttonFieldMan.add(connectField);
         add(buttonFieldMan);
     }
 
-
-    public boolean keyChar(char c, int status, int time) {
-        if (c == Characters.ESCAPE) {
-            close();
-            return true;    
-        }
-        return super.keyChar(c, status, time);
-    }
 }

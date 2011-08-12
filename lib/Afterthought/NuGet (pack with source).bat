531090cfd4ac8b7983323b237a3eac111cf52902
Nithin Philips <nithin@nithinphilips.com>
2011-08-12 03:51:47 -0400

    Removing Afterthought. We will use git submodules instead.

--- lib/Afterthought/NuGet (pack with source).bat
+++
@@ -1,2 +0,0 @@
-nuget pack -sym Afterthought.Amender\Afterthought.Amender.csproj
-copy *.nupkg \\vc3-cae-tfs-01\d$\NuGetServer\Packages
--- lib/Afterthought/NuGet (pack without source).bat
+++
@@ -1,2 +0,0 @@
-nuget pack Afterthought.Amender\Nuget\Afterthought.nuspec
-copy *.nupkg \\vc3-cae-tfs-01\d$\NuGetServer\Packages

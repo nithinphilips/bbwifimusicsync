c53e3c73ffc3063c0f49c6d0cd483ae269b1ee83
Nithin Philips <nithin@nithinphilips.com>
2011-08-16 01:05:31 -0400

    Copying installer from FlashCardmaster.

---
+++ Build-Release.bat
@@ -0,0 +1,3 @@
+@Echo off
+Rem You need .NET framework SDK for this to work
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /property:Configuration=Release
---
+++ MusicSync.Installer/sign.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\signtool.exe" sign /v /s My /i "Nithin Philips Software" /t http://timestamp.verisign.com/scripts/timestamp.dll "*.exe"

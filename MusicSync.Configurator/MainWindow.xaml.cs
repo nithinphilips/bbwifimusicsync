4efd74616d1d36acd1ff1e1c9e511bb6567fa481
Nithin Philips <nithin@nithinphilips.com>
2011-10-25 01:57:58 -0400

    Adding an app to configure Wireless Music Sync.

---
+++ MusicSync.Configurator/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MusicSync.Configurator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SettingsModel settings;
+
+        public MainWindow()
+        {
+            settings = new SettingsModel();
+            settings.Load();
+            this.DataContext = settings;
+            
+            InitializeComponent();
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            settings.Save();
+            Application.Current.Shutdown(0);
+        }
+    }
+}
---
+++ MusicSync.Configurator/SettingsModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using NotifyPropertyChanged;
+
+namespace MusicSync.Configurator
+{
+    [NotifyPropertyChanged]
+    class SettingsModel
+    {
+        public int Port { get; set; }
+        public string iTunesLibraryPath { get; set; }
+        public bool OneWaySync { get; set; }
+        public bool AllowPlayerControl { get; set; }
+
+        public void Load()
+        {
+            var config = GetConfigReader("libMusicSync.dll");
+            iTunesLibraryPath = GetAppSetting(config, "iTunesLibraryPath");
+
+            //config = GetConfigReader("MusicSync.Server.exe");
+            //int port = 0;
+            //if(int.TryParse(GetAppSetting(config, "Port"), out port))
+            //{
+            //    Port = port;
+            //}
+
+            //bool oneWaySync = false;
+            //if (bool.TryParse(GetAppSetting(config, "OneWaySync"), out oneWaySync))
+            //{
+            //    OneWaySync = oneWaySync;
+            //}
+
+            //bool allowPlayerControl = false;
+            //if(bool.TryParse(GetAppSetting(config, "AllowPlayerControl"), out allowPlayerControl))
+            //{
+            //    AllowPlayerControl = allowPlayerControl;
+            //}
+        }
+
+        public void Save()
+        {
+            var config = GetConfigReader("libMusicSync.dll");
+            SetAppSetting(config, "iTunesLibraryPath", iTunesLibraryPath);
+            config.Save();
+        }
+
+        static string GetAppSetting(Configuration config, string key)
+        {
+            KeyValueConfigurationElement element = config.AppSettings.Settings[key];
+            if (element != null)
+            {
+                string value = element.Value;
+                if (!string.IsNullOrEmpty(value))
+                    return value;
+            }
+            return string.Empty;
+        }
+
+        static bool SetAppSetting(Configuration config, string key, string value)
+        {
+            KeyValueConfigurationElement element = config.AppSettings.Settings[key];
+            if (element != null)
+            {
+                element.Value = value;
+                return true;
+            }
+            return false;
+        }
+
+        static Configuration GetConfigReader(string exeConfigPath)
+        {
+            Configuration config = null;
+            try
+            {
+                config = ConfigurationManager.OpenExeConfiguration(exeConfigPath);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+            return config;
+        }
+    }
+}
